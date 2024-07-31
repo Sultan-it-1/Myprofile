fd8dac7b0939086abea8a6c7677a9e03d94f9596
Sultan-it-1 <sultanayidh123@gmail.com>
2024-07-31 18:37:06 +0300

    Update contact.js

--- contact/contact.js
+++ contact/contact.js
@@ -1,12 +1,14 @@
-// contact.js
+document.addEventListener('DOMContentLoaded', function() {
+    emailjs.init("8M2Q0XU-H2L2q14TE");
 
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-            event.preventDefault();
+    document.getElementById('contactForm').addEventListener('submit', function(event) {
+        event.preventDefault();
 
-            emailjs.sendForm('service_kbcg0i1', '8M2Q0XU-H2L2q14TE', this)
-                .then(function() {
-                    alert('تم إرسال الرسالة بنجاح!');
-                }, function(error) {
-                    alert('حدث خطأ أثناء إرسال الرسالة: ' + JSON.stringify(error));
-                });
-        });
+        emailjs.sendForm('service_kbcg0i1', 'template_dn2vwll', this)
+            .then(function() {
+                alert('تم إرسال الرسالة بنجاح!');
+            }, function(error) {
+                alert('حدث خطأ أثناء إرسال الرسالة: ' + JSON.stringify(error));
+            });
+    });
+});
