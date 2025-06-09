edd6bdedb40b69884ecc06f8138805712e66add1
Sultan-it-1 <sultanayidh123@gmail.com>
2025-06-09 13:39:18 +0300

    Update fingerprint.js

--- try/fingerprint.js
+++ try/fingerprint.js
@@ -1,16 +1,11 @@
-// fingerprint.js
-
-// Initialize the agent on page load.
 const fpPromise = import('https://fpjscdn.net/v3/XrDRrmVoT8HUwLg1yH5Q')
   .then(FingerprintJS => FingerprintJS.load({
     region: "eu"
   }));
 
-// Get the visitorId when you need it.
 fpPromise
   .then(fp => fp.get())
   .then(result => {
     const visitorId = result.visitorId;
     console.log(visitorId);
   });
-
